c4f8b1f58cc67db5bf387a87ca4cc2bc77a0a00a
IkezoeMakoto <150283t@gmail.com>
2020-09-06 15:48:51 +0900

    build通した

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -327,14 +327,13 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 
 	type Data struct {
 		Keyword string
-		Hash string
-		Url string
+		Hash    string
+		Url     string
 	}
 
 	keywords2 := make([]Data, 0, 500)
 
 	keywords := make([]string, 0, 500)
-
 
 	for _, entry := range entries {
 
@@ -342,26 +341,26 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 
 		kw := regexp.QuoteMeta(entry.Keyword)
 
-		d:= Data{ 
+		path,_ := r.URL.Parse(baseUrl.String()+"/keyword/"+pathURIEscape(kw))
+		d := Data{
 			Keyword: kw,
-			Hash: "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-			Url: fmt.Sprintf("<a href=\"%s\">%s</a>", r.URL.Parse(baseUrl.String()+"/keyword/" + pathURIEscape(kw)), html.EscapeString(kw))
+			Hash:    "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw))),
+			Url:     fmt.Sprintf("<a href=\"%s\">%s</a>", path, html.EscapeString(kw)),
 		}
-
-		keywords2 = append(keywords,d)	
+		keywords2 = append(keywords2, d)
 	}
-	re := regexp.MustCompile("("+strings.Join(keywords, "|")+")")
+	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
 	kw2sha := make(map[string]string)
 	content = re.ReplaceAllStringFunc(content, func(kw string) string {
 		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
 		return kw2sha[kw]
 	})
 	content = html.EscapeString(content)
-	log.Println("kw2sha:"+ kw2sha)
+	log.Println("kw2sha:", kw2sha)
 	log.Println("~~~~~~~~~~~~")
 	log.Println(keywords2)
 	for kw, hash := range kw2sha {
-		u, err := r.URL.Parse(baseUrl.String()+"/keyword/" + pathURIEscape(kw))
+		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
 		panicIf(err)
 		link := fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw))
 		content = strings.Replace(content, hash, link, -1)
